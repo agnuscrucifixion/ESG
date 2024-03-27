5038c91a7092e2ba364bf3573e622ea645c85e21
agnuscrucifixion <nikitabysko123@gmail.com>
2024-03-27 23:16:12 +0300

    client ESG

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
 import os
-import easyOCR
-import apryse
-import util
+import requests
 
 
-def main(path):
+def main(url, path):
     print("Начинается обработка")
-    if not os.path.exists("temp"):
-        os.mkdir("temp")
-        os.mkdir("final")
-        os.mkdir("temp/images")
-        os.mkdir("temp/apryse_text")
-        os.mkdir("temp/ocr_text")
-    apryse.convert_to_text(path)
-    #easyOCR.to_png(path)
-    #easyOCR.process_images()
-    #util.final_coupling('final/ocr_text.txt', 'temp/apryse_text')
-    util.clean_after()
+    files = {'file': open(path, 'rb')}
+    response = requests.post(url, files=files)
+    if response.status_code == 200:
+        with open('final.txt', 'wb') as f:
+            f.write(response.content)
+            print("Text file downloaded successfully.")
+            return True
+    else:
+        print("Failed to download text file.")
+        return False
 
 
 if __name__ == '__main__':
     print("Введите аргумент: ")
     argument = input()
     print("\n")
-    while not util.check_argument(argument):
+    while not main("http://localhost:5000/upload-pdf", argument):
         print("\n")
         print("Пожалуйста, введите корректный аргумент: ")
         argument = input()
         print("\n")
 
-    print(f"Принятый аргумент: {argument}")
-    print("\n")
-    main(argument)
