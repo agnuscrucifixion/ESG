2958a0625b20af911164939dda512d43a0559293
agnuscrucifixion <nikitabysko123@gmail.com>
2024-04-04 10:46:16 +0300

    upgrade logic

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import requests
 import docker
+import os
 
-#client = docker.from_env()
+
+# client = docker.from_env()
+
+def before():
+    if not os.path.exists("final"):
+        os.mkdir("final")
 
 
 def main(url, path):
+    before()
     print("Начинается обработка")
     files = {'file': open(path, 'rb')}
+    name_pdf = os.path.splitext(os.path.basename(path))[0]
     response = requests.post(url, files=files)
     if response.status_code == 200:
-        with open('final.txt', 'wb') as f:
+        with open(f'final/{name_pdf}.txt', 'wb') as f:
             f.write(response.content)
             print("Text file downloaded successfully.")
             requests.get("http://localhost:5000/clean")
@@ -20,9 +28,9 @@ def main(url, path):
 
 
 if __name__ == '__main__':
-    #image = client.images.pull('agnusdecrucifixion/pdf_converter:prefinal')
-    #container = client.containers.run('agnusdecrucifixion/pdf_converter:prefinal', ports={'5000/tcp': 5000}, detach=True)
-    #print(container.id)
+    # image = client.images.pull('agnusdecrucifixion/pdf_converter:prefinal')
+    # container = client.containers.run('agnusdecrucifixion/pdf_converter:prefinal', ports={'5000/tcp': 5000}, detach=True)
+    # print(container.id)
     print("Введите аргумент: ")
     argument = input()
     print("\n")
@@ -31,4 +39,3 @@ if __name__ == '__main__':
         print("Пожалуйста, введите ешё: ")
         argument = input()
         print("\n")
-
